bb077ea5c2631a21637637625d0d46c35695dbf9
suspiciousboy-bots <suspiciousboy1412@gmail.com>
2026-05-29 13:06:38 +0800

    Update config.py
    Signed-off-by: suspiciousboy-bots <suspiciousboy1412@gmail.com>

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
         self.BOT_TOKEN = getenv("BOT_TOKEN")
         self.MONGO_URL = getenv("MONGO_URL")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.LOGGER_ID = int(getenv("LOGGER_ID", "-1003401639448"))
+        self.OWNER_ID = int(getenv("OWNER_ID", "7065156396"))
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
@@ -37,9 +37,9 @@ class Config:
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://iili.io/C2NSKPe.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://iili.io/C2NeQne.jpg")
+        self.START_IMG = getenv("START_IMG", "https://iili.io/C2NNWiJ.jpg")
 
     def check(self):
         missing = [
